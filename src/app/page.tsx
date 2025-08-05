c5200e0c7d4470c9f45c2844e31440d9812bd149
kauamassei <gameplayultra11@gmail.com>
2025-08-05 01:39:29 -0300

    chore: update shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
+    <>
     <div className='text-red-700'>page</div>
+    <Button>Bootcamp</Button>
+    </>
   )
 }
 
