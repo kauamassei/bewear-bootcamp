95b642ba188cbc648e1f90351e0fafe5027dddf6
kauamassei <gameplayultra11@gmail.com>
2025-08-07 02:43:05 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,47 @@
 import Header from "@/components/common/header";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
+import { db } from "@/db";
+import ProductList from "@/components/common/product-list";
 
-const page = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  console.log(products);
   return (
     <>
-     <Header />
+      <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vh"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Seja autêntico"
+            height={0}
+            width={0}
+            sizes="100vh"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 };
 
-export default page;
+export default Home;
