b9422a08089a9e59916e48a724f8fe230bfedda8
kauamassei <gameplayultra11@gmail.com>
2025-08-09 03:22:08 -0300

    feat: add product page

---
+++ src/app/product-variant/[slug]/components/variant-selector.tsx
@@ -0,0 +1,36 @@
+import { productVariantTable } from "@/db/schema";
+import Image from "next/image";
+import Link from "next/link";
+
+
+interface VariantSelectorProps {
+    selectedVariantSlug: string,
+  variants: (typeof productVariantTable.$inferSelect)[];
+}
+
+const VariantSelector = ({ variants, selectedVariantSlug }: VariantSelectorProps) => {
+    
+  return (
+    <>
+      <div className="flex items-center gap-4">
+        {variants.map((variant) => (
+
+
+          <Link href={`/product-variant/${variant.slug}`} key={variant.id}
+          className={selectedVariantSlug === variant.slug ? 'border-primary rounded-xl border-2' : ''}
+          >
+            <Image
+              width={68}
+              height={68}
+              src={variant.imageUrl}
+              alt={variant.name}
+              className="rounded-xl"
+            />
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default VariantSelector;
--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -9,28 +9,30 @@ interface ProductItemProps {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
   textContainerClassName?: string;
-
 }
 const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   // Corrigindo o tipo de 'product' para ser um único produto, não um array
   const firstVariant = product.variants[0];
   return (
     <>
-      <Link href="/" className="flex flex-col gap-4">
+      <Link
+        href={`/product-variant/${firstVariant.slug}`}
+        className="flex flex-col gap-4"
+      >
         <Image
           src={firstVariant.imageUrl}
           alt={firstVariant.name}
           sizes="100vh"
           width={0}
           height={0}
-          className="rounded-3xl h-auto w-full"
+          className="h-auto w-full rounded-3xl"
         />
-         <div
-        className={cn(
-          "flex max-w-[200px] flex-col gap-1",
-          textContainerClassName,
-        )}
-      >
+        <div
+          className={cn(
+            "flex max-w-[200px] flex-col gap-1",
+            textContainerClassName,
+          )}
+        >
           <p className="truncate text-sm font-medium">{product.name}</p>
           <p className="text-muted-foreground truncate text-xs font-medium">
             {product.description}
